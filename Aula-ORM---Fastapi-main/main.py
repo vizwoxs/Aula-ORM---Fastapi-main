026d5e0f7bde437ef509fa77043e85448c4f5df0
VIk <vikoliveira.dev@gmail.com>
2026-04-13 16:21:07 -0300

    Criando as tabelas e o inico do html

--- Aula-ORM---Fastapi-main/main.py
+++ Aula-ORM---Fastapi-main/main.py
@@ -1,23 +1,24 @@
 from fastapi import FastAPI, Depends, HTTPException, Request, Form
+from fastapi.responses import RedirectResponse
+from fastapi.templating import Jinja2Templates
+from sqlalchemy.orm import Session 
+from database import get_db
+from models import Curso, Aluno
 
 # Inicializar o fastapi
 app = FastAPI(title="Gestão escolar")
+templates = Jinja2Templates(directory="templates")
 
 # Rodar api
 # No terminal: python -m uvicorn main:app --reload
 
 # Rota
 # Métodos http: GET, POST, PUT, DELETE
-@app.get("/")
-def tela_inicial():
-    return {"mensagem":"Bem-vindo ao sistema de gestão escolar"}
 
-# Banco de dados
-alunos = {
-    1:{"nome": "Gabriel", "idade": 34},
-    2:{"nome": "Cassio", "idade": 34},
-    3:{"nome": "Emilly", "idade": 34},
-}
-@app.get("/alunos")
-def listar_alunos():
-    return {"alunos":alunos}
+@app.get("/cursos/cadastro")
+def cadastro_curso(request: Request):
+    return templates.TemplateResponse(
+        request,
+        "cadastro_curso.html",
+        {"request": request}
+    )
